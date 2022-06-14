6fa0a0279427b5cd43a4f8310cd35fa82005bc1d
Alia Hidayah <56750777+aliaahidd@users.noreply.github.com>
2022-06-15 01:54:22 +0800

    Update dbase.php

--- connection/dbase.php
+++ connection/dbase.php
@@ -1,4 +1,4 @@
 <?php
-$con=mysqli_connect("localhost","root","","foody") or die(mysqli_error($con));
+$con=mysqli_connect("us-cdbr-east-05.cleardb.net","baba0f1acf6887","709c1250","foody") or die(mysqli_error($con));
 date_default_timezone_set('Asia/Kuala_Lumpur');
 ?>
